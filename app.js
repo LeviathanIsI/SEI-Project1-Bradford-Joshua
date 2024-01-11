d07b121184dc6c013c8764bf008c5e648479d45f
LeviathanIsI <joshua.r.bradford1@gmail.com>
2024-01-10 20:58:27 -0500

    Initialized buttons using DOM

---
+++ app.js
@@ -0,0 +1,8 @@
+//Initialize Buttons
+let topButtons = document.querySelector("topbuttons");
+let bottomButtons = document.querySelector("bottombuttons");
+let greenButton = document.querySelector("greenbutton");
+let redButton = document.querySelector("redbutton");
+let yellowButton = document.querySelector("yellowbutton");
+let blueButton = document.querySelector("bluebutton");
+
