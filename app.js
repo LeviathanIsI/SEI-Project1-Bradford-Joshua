0b6bf142713a2b844a996b6cefe7a1eb35ce1212
LeviathanIsI <joshua.r.bradford1@gmail.com>
2024-01-10 21:04:35 -0500

    Added event listeners and named functions

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ let redButton = document.querySelector("redbutton");
 let yellowButton = document.querySelector("yellowbutton");
 let blueButton = document.querySelector("bluebutton");
 
+//Add event listeners to each button
+greenButton.addEventListener("click");
+redButton.addEventListener("click");
+yellowButton.addEventListener("click");
+blueButton.addEventListener("click");
+
+//Provides a progressive sequence for the user to mimic
+function randomSequence() {}
+
+//Listens for and compares the user input to the generated sequence
+function userInput() {}
+
+//Check to see if the provide sequence is valid
+function invalidSequence() {}
+
+//Timer function between button clicks
+function timer() {}
