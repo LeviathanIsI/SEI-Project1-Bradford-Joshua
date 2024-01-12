b867691fd7cf62544cfe533c97cababeeb3cdfa6
LeviathanIsI <joshua.r.bradford1@gmail.com>
2024-01-11 20:13:09 -0500

    adding start of random sequence function

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 //Initialize Buttons
-let topButtons = document.querySelector("topbuttons");
-let bottomButtons = document.querySelector("bottombuttons");
-let greenButton = document.querySelector("greenbutton");
-let redButton = document.querySelector("redbutton");
-let yellowButton = document.querySelector("yellowbutton");
-let blueButton = document.querySelector("bluebutton");
+let topButtons = document.querySelector(".topbuttons");
+let bottomButtons = document.querySelector(".bottombuttons");
+let greenButton = document.querySelector(".greenbutton");
+let redButton = document.querySelector(".redbutton");
+let yellowButton = document.querySelector(".yellowbutton");
+let blueButton = document.querySelector(".bluebutton");
 
-//Add event listeners to each button
-greenButton.addEventListener("click");
-redButton.addEventListener("click");
-yellowButton.addEventListener("click");
-blueButton.addEventListener("click");
+// Add event listeners to each button
+// greenButton.addEventListener("click", userInput);
+// redButton.addEventListener("click", userInput);
+// yellowButton.addEventListener("click", userInput);
+// blueButton.addEventListener("click", userInput);
+
+let buttons = [greenButton, redButton, yellowButton, blueButton];
+let randomButtonSequence = [];
 
 //Provides a progressive sequence for the user to mimic
-function randomSequence() {}
+function randomSequence() {
+  let randomIndex = Math.floor(Math.random() * buttons.length);
+  let randomButton = buttons[randomIndex];
+  randomButtonSequence.push(randomButton);
+}
 
-//Listens for and compares the user input to the generated sequence
+// Listens for and compares the user input to the generated sequence
 function userInput() {}
 
-//Check to see if the provide sequence is valid
+// Check to see if the provide sequence is valid
 function invalidSequence() {}
 
-//Timer function between button clicks
+// Timer function between button clicks
 function timer() {}
+
+randomSequence();
+console.log(randomButtonSequence);
